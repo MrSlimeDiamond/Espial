f7472822ac534bdf931064d160799f0b9d3e2601
AlexAdvDev <38597038+AlexAdvDev@users.noreply.github.com>
2024-07-12 22:47:28 -0600

    Update EspialConfiguration.java

--- src/main/java/net/slimediamond/espial/EspialConfiguration.java
+++ src/main/java/net/slimediamond/espial/EspialConfiguration.java
@@ -1,13 +1,1 @@
-package net.slimediamond.espial;
 
-import org.spongepowered.configurate.objectmapping.ConfigSerializable;
-import org.spongepowered.configurate.objectmapping.meta.Setting;
-
-@ConfigSerializable
-public class EspialConfiguration {
-    @Setting private String jdbc = "jdbc:sqlite:espial.db";
-
-    public String jdbc() {
-        return this.jdbc;
-    }
-}
