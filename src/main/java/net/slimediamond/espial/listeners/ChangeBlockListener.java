bcf5fb61bfbda504375bd0ac20ecfc444171a27d
Findlay Richardson <findlayrichardson3@gmail.com>
2025-02-07 19:01:32 +1300

    Log slightly less useless garbage

--- src/main/java/net/slimediamond/espial/listeners/ChangeBlockListener.java
+++ src/main/java/net/slimediamond/espial/listeners/ChangeBlockListener.java
@@ -3,10 +3,13 @@ package net.slimediamond.espial.listeners;
 import net.slimediamond.espial.ActionType;
 import net.slimediamond.espial.Database;
 import org.checkerframework.checker.nullness.qual.Nullable;
+import org.spongepowered.api.Server;
+import org.spongepowered.api.block.transaction.Operations;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.block.ChangeBlockEvent;
 import org.spongepowered.api.event.block.InteractBlockEvent;
+import org.spongepowered.api.world.server.ServerWorld;
 
 import java.sql.SQLException;
 
@@ -34,6 +37,10 @@ public class ChangeBlockListener {
         }
 
         event.transactions().forEach(transaction -> {
+            // These are almost always useless, and just flood the database.
+            // It's stuff like "this water spread"
+            if (transaction.operation().equals(Operations.MODIFY.get())) return;
+
             try {
                 database.insertAction(
                         ActionType.fromOperation(transaction.operation()),
