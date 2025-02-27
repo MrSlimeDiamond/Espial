4d9e89c20dcf6b99f5e7ad7f83084a736e887485
MrSlimeDiamond <findlayrichardson3@gmail.com>
2025-02-27 17:39:08 +1300

    Do not allow cancellation of EspialPostInsertRecordEvent
    It's already inserted!

--- src/main/java/net/slimediamond/espial/api/event/EspialPostInsertRecordEvent.java
+++ src/main/java/net/slimediamond/espial/api/event/EspialPostInsertRecordEvent.java
@@ -3,7 +3,7 @@ package net.slimediamond.espial.api.event;
 import net.slimediamond.espial.api.record.EspialRecord;
 import net.slimediamond.espial.api.user.EspialActor;
 
-public class EspialPostInsertRecordEvent extends AbstractCancellable {
+public class EspialPostInsertRecordEvent extends AbstractEvent {
   private EspialRecord record;
 
   public EspialPostInsertRecordEvent(EspialActor actor, EspialRecord record) {
