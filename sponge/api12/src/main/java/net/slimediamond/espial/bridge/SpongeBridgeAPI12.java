664afca943fc7066576643eefc388933b12132cd
MrSlimeDiamond <findlayrichardson3@gmail.com>
2025-03-11 21:50:33 +1300

    Fix SpongeAPI 12 support

--- sponge/api12/src/main/java/net/slimediamond/espial/bridge/SpongeBridgeAPI12.java
+++ sponge/api12/src/main/java/net/slimediamond/espial/bridge/SpongeBridgeAPI12.java
@@ -1,10 +1,21 @@
 package net.slimediamond.espial.bridge;
 
 import org.spongepowered.api.event.block.ChangeBlockEvent;
+import org.spongepowered.api.event.block.InteractBlockEvent;
+import org.spongepowered.api.event.item.inventory.InteractItemEvent;
 
 public class SpongeBridgeAPI12 implements SpongeBridge {
   @Override
   public Object getRootCause(ChangeBlockEvent event) {
-    return event.cause().root();
+    Object source = event.cause().root();
+    if (event.cause().root() instanceof InteractBlockEvent.Primary.Start) {
+      source = ((InteractBlockEvent.Primary) event.cause().root()).source();
+    } else if (event.cause().root() instanceof InteractBlockEvent.Secondary) {
+      source = ((InteractBlockEvent.Secondary) event.cause().root()).source();
+    } else if (event.cause().root() instanceof InteractItemEvent.Secondary) {
+      source = ((InteractItemEvent.Secondary) event.cause().root()).source();
+    }
+
+    return source;
   }
 }
