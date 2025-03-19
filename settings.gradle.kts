eab020cead6b140c04589643f5559cdaa4a3c41e
MrSlimeDiamond <findlayrichardson3@gmail.com>
2025-03-19 15:29:02 +1300

    Remove old modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,2 @@
 rootProject.name = "espial"
 include("api")
-include("sponge:api12")
-findProject(":sponge:api12")?.name = "api12"
-include("sponge:api14")
-findProject(":sponge:api14")?.name = "api14"
