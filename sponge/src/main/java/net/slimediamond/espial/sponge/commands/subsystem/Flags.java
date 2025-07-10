3c5db581bd19862fad2c6fc4d697c50dda1eaab1
MrSlimeDiamond <findlayrichardson3@gmail.com>
2025-07-11 11:00:20 +1200

    Give each flag only one alias
    seems to be a Sponge bug or something which breaks client completion

--- sponge/src/main/java/net/slimediamond/espial/sponge/commands/subsystem/Flags.java
+++ sponge/src/main/java/net/slimediamond/espial/sponge/commands/subsystem/Flags.java
@@ -5,12 +5,12 @@ import org.spongepowered.api.command.parameter.managed.Flag;
 public class Flags {
 
     public static final Flag SPREAD = Flag.of(Parameters.SPREAD, "spread", "s");
-    public static final Flag AFTER = Flag.of(Parameters.AFTER, "after", "a", "t");
+    public static final Flag AFTER = Flag.of(Parameters.AFTER, "t");
     public static final Flag BEFORE = Flag.of(Parameters.BEFORE, "before"); // 'b' is reserved for block
-    public static final Flag PLAYER = Flag.of(Parameters.USER, "player", "p");
-    public static final Flag EVENT = Flag.of(Parameters.EVENT, "event", "e");
-    public static final Flag BLOCK = Flag.of(Parameters.BLOCK_TYPE, "block", "b");
-    public static final Flag WORLDEDIT = Flag.of(Parameters.WORLDEDIT, "worldedit", "w");
+    public static final Flag PLAYER = Flag.of(Parameters.USER, "p");
+    public static final Flag EVENT = Flag.of(Parameters.EVENT, "e");
+    public static final Flag BLOCK = Flag.of(Parameters.BLOCK_TYPE, "b");
+    public static final Flag WORLDEDIT = Flag.of(Parameters.WORLDEDIT, "w");
     public static final Flag YES = Flag.of(Parameters.YES, "yes");
 
 }
