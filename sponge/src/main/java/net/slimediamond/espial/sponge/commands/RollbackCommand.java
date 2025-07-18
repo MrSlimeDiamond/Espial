d22bf0a5aa82592bd228786916ff536878b8f721
MrSlimeDiamond <findlayrichardson3@gmail.com>
2025-07-19 11:20:40 +1200

    Fix rollback permission/description

--- sponge/src/main/java/net/slimediamond/espial/sponge/commands/RollbackCommand.java
+++ sponge/src/main/java/net/slimediamond/espial/sponge/commands/RollbackCommand.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class RollbackCommand extends TransactionCommand {
 
     public RollbackCommand() {
-        super(TransactionTypes.ROLLBACK.get(), Permissions.RESTORE, Component.text("Restore a selection"));
+        super(TransactionTypes.ROLLBACK.get(), Permissions.ROLLBACK, Component.text("Rollback a selection"));
 
         addAlias("rollback");
         addAlias("rb");
