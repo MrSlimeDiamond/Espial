e6b67f1fd3f59e1d4f5f5ff3ab052608b36c1867
MrSlimeDiamond <findlayrichardson3@gmail.com>
2025-07-19 10:20:39 +1200

    Improved interactive mode

--- sponge/src/main/java/net/slimediamond/espial/sponge/listeners/SpongeListeners.java
+++ sponge/src/main/java/net/slimediamond/espial/sponge/listeners/SpongeListeners.java
@@ -13,13 +13,16 @@ import org.spongepowered.api.block.BlockSnapshot;
 import org.spongepowered.api.block.transaction.BlockTransaction;
 import org.spongepowered.api.block.transaction.Operation;
 import org.spongepowered.api.command.manager.CommandMapping;
-import org.spongepowered.api.data.Keys;
+import org.spongepowered.api.data.type.HandTypes;
 import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.hanging.Hanging;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.entity.living.player.server.ServerPlayer;
+import org.spongepowered.api.event.EventContextKeys;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.Order;
 import org.spongepowered.api.event.block.ChangeBlockEvent;
+import org.spongepowered.api.event.block.InteractBlockEvent;
 import org.spongepowered.api.event.block.entity.ChangeSignEvent;
 import org.spongepowered.api.event.entity.DestructEntityEvent;
 import org.spongepowered.api.event.filter.cause.First;
@@ -30,9 +33,26 @@ import org.spongepowered.api.world.server.ServerLocation;
 import org.spongepowered.plugin.PluginContainer;
 
 import java.util.Optional;
-import java.util.UUID;
 
 public class SpongeListeners {
+
+    @Listener(order = Order.EARLY)
+    public void onBlockChange(final InteractBlockEvent.Primary.Start event, @First ServerPlayer player) {
+        if (Espial.getInstance().getEspialService().getInspectingUsers().contains(player.uniqueId())) {
+            event.block().location().ifPresent(location -> {
+                event.setCancelled(true);
+                queryRecords(location, player);
+            });
+        }
+    }
+
+    @Listener(order = Order.EARLY)
+    public void onBlockChange(final InteractBlockEvent.Secondary event, @First ServerPlayer player) {
+        if (Espial.getInstance().getEspialService().getInspectingUsers().contains(player.uniqueId())
+                && event.context().get(EventContextKeys.USED_HAND).map(h -> h.equals(HandTypes.MAIN_HAND.get())).orElse(false)) {
+            event.block().location().ifPresent(location -> queryRecords(location.relativeTo(event.targetSide()), player));
+        }
+    }
 
     @Listener
     public void onBlockChange(final ChangeBlockEvent.All event) {
@@ -52,29 +72,12 @@ public class SpongeListeners {
                 continue; // somehow no location
             }
             final Optional<ServerPlayer> playerOptional = event.cause().first(ServerPlayer.class);
-            if (playerOptional.isPresent()) {
-                final UUID uuid = playerOptional.get().uniqueId();
-                if (Espial.getInstance().getEspialService().getInspectingUsers().contains(uuid)) {
-                    event.setCancelled(true);
-                    // lookup at those coordinates
-                    final ServerLocation location = transaction.original().location().get();
-                    Espial.getInstance().getEspialService().query(EspialQuery.builder()
-                                    .location(location)
-                                    .audience(playerOptional.get())
-                                    .build())
-                            .thenAccept(records -> {
-                                if (records.isEmpty()) {
-                                    playerOptional.get().sendMessage(Format.NO_RECORDS_FOUND);
-                                } else {
-                                    PaginationList.builder()
-                                            .title(Format.title("Lookup results"))
-                                            .padding(Format.PADDING)
-                                            .contents(RecordFormatter.formatRecords(records, true))
-                                            .sendTo(playerOptional.get());
-                                }
-                            });
-                    return;
-                }
+
+            if (playerOptional.isPresent()
+                    && Espial.getInstance().getEspialService().getInspectingUsers().contains(playerOptional.get().uniqueId())) {
+                event.setCancelled(true);
+                queryRecords(replacement.location().get(), playerOptional.get());
+                return;
             }
 
             final Optional<EspialEvent> eventOptional = getEspialEvent(transaction.operation());
@@ -132,6 +135,24 @@ public class SpongeListeners {
                 .filter(entry -> entry.key().value().equals(operation.key(RegistryTypes.OPERATION).value()))
                 .map(RegistryEntry::value)
                 .findFirst();
+    }
+
+    private static void queryRecords(final ServerLocation location, final Player player) {
+        Espial.getInstance().getEspialService().query(EspialQuery.builder()
+                        .location(location)
+                        .audience(player)
+                        .build())
+                .thenAccept(records -> {
+                    if (records.isEmpty()) {
+                        player.sendMessage(Format.NO_RECORDS_FOUND);
+                    } else {
+                        PaginationList.builder()
+                                .title(Format.title("Lookup results"))
+                                .padding(Format.PADDING)
+                                .contents(RecordFormatter.formatRecords(records, true))
+                                .sendTo(player);
+                    }
+                });
     }
 
 }
