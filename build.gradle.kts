8746c61a82b765ec4d80dadc54cb832749abf542
MrSlimeDiamond <findlayrichardson3@gmail.com>
2025-07-20 11:38:02 +1200

    Fix GitLab publication repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,16 +6,18 @@ allprojects {
     plugins.apply("maven-publish")
     group = "net.slimediamond"
     version = "2.0r-SNAPSHOT"
-    repositories {
-        maven {
-            name = "GitLab"
-            url = uri("https://gitlab.com/api/v4/projects/" + System.getenv("CI_PROJECT_ID") + "/packages/maven")
-            credentials(HttpHeaderCredentials::class) {
-                name = "Job-Token"
-                value = System.getenv("CI_JOB_TOKEN") // deploy packages should only be created by a CI job
-            }
-            authentication {
-                create("header", HttpHeaderAuthentication::class)
+    publishing {
+        repositories {
+            maven {
+                name = "GitLab"
+                url = uri("https://gitlab.com/api/v4/projects/" + System.getenv("CI_PROJECT_ID") + "/packages/maven")
+                credentials(HttpHeaderCredentials::class) {
+                    name = "Job-Token"
+                    value = System.getenv("CI_JOB_TOKEN") // deploy packages should only be created by a CI job
+                }
+                authentication {
+                    create("header", HttpHeaderAuthentication::class)
+                }
             }
         }
     }
